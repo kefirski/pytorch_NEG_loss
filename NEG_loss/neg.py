c8293d0c454cb89bd33e6a92b47feda19208abbf
Daniil Gavrilov <olavursky@gmail.com>
2017-03-04 10:43:07 +0300

    add module

---
+++ NEG_loss/neg.py
@@ -0,0 +1,58 @@
+import torch as t
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class NEG_loss(nn.Module):
+    def __init__(self, num_classes, embed_size):
+        """
+        :param num_classes: An int. The number of possible classes.
+        :param embed_size: An int. Embedding size
+        """
+
+        super(NEG_loss, self).__init__()
+
+        self.num_classes = num_classes
+        self.embed_size = embed_size
+
+        self.out_embed = nn.Embedding(self.num_classes, self.embed_size)
+        self.out_embed.weight = t.nn.Parameter(t.FloatTensor(self.num_classes, self.embed_size).uniform_(-1, 1))
+
+        self.in_embed = nn.Embedding(self.num_classes, self.embed_size)
+        self.in_embed.weight = t.nn.Parameter(t.FloatTensor(self.num_classes, self.embed_size).uniform_(-1, 1))
+
+    def forward(self, input_labels, out_labels, num_sampled):
+        """
+        :param input_labels: Tensor with shape of [batch_size] of Long type
+        :param out_labels: Tensor with shape of [batch_size] of Long type
+        :param num_sampled: An int. The number of sampled from noise examples
+
+        :return: Loss estimation with shape of [batch_size]
+            loss defined in Mikolov et al. Distributed Representations of Words and Phrases and their Compositionality
+            https://arxiv.org/abs/1310.4546
+        """
+
+        use_cuda = self.out_embed.weight.is_cuda
+
+        [batch_size] = input_labels.size()
+
+        input = self.in_embed(input_labels)
+        output = self.out_embed(out_labels)
+
+        noise = Variable(t.Tensor(batch_size, num_sampled).uniform_(0, self.num_classes - 1).long())
+        if use_cuda:
+            noise = noise.cuda()
+        noise = self.out_embed(noise).neg()
+
+        log_target = (input * output).sum(1).squeeze(1).sigmoid().log()
+
+        ''' ∑[batch_size, num_sampled, embed_size] * [batch_size, embed_size, 1] ->
+            ∑[batch_size, num_sampled] -> [batch_size] '''
+        sum_log_sampled = t.bmm(noise, input.unsqueeze(2)).sigmoid().log().sum(1).squeeze(1)
+
+        loss = log_target + sum_log_sampled
+
+        return -loss
+
+    def input_embeddings(self):
+        return self.in_embed.weight.data.cpu().numpy()
---
+++ README.md
@@ -0,0 +1,5 @@
+# Pytorch Negative Sampling Loss
+
+[Negative Sampling Loss](https://arxiv.org/abs/1310.4546) implemented in [PyTorch](http://www.pytorch.org).
+
+![NEG Loss Equation](images/neg.png)
