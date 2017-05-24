8f4c374b88ac82f2dc58247d4c6d10546563fd03
patrick-dd <patrick.doupe@gmail.com>
2017-05-24 17:06:41 -0400

    added subsampling instead of uniform dist

--- NEG_loss/neg.py
+++ NEG_loss/neg.py
@@ -5,10 +5,15 @@ from torch.nn import Parameter
 
 
 class NEG_loss(nn.Module):
-    def __init__(self, num_classes, embed_size):
+    def __init__(self, num_classes, embed_size, classes, weights):
         """
         :param num_classes: An int. The number of possible classes.
         :param embed_size: An int. EmbeddingLockup size
+        :param classes: A list of ints. Class identifiers
+        :param num_sampled: An int. The number of sampled from noise examples
+        :param weights: A list of floats. Class weights
+            The weights are calculated prior to estimation and can be of any form,
+            e.g equation (5) in [1]
         """
 
         super(NEG_loss, self).__init__()
@@ -21,6 +26,18 @@ class NEG_loss(nn.Module):
 
         self.in_embed = nn.Embedding(self.num_classes, self.embed_size)
         self.in_embed.weight = Parameter(t.FloatTensor(self.num_classes, self.embed_size).uniform_(-1, 1))
+
+        # FOR SUBSAMPLING
+        self.classes = classes
+        self.weights = weights
+        self.n_samples = n_samples
+
+    def subsample(self, n_samples):
+        """
+        draws a sample from classes based on weights
+        """
+        draw = choice(self.classes, n_samples, self.weights)
+        return np.array(draw)
 
     def forward(self, input_labes, out_labels, num_sampled):
         """
@@ -38,8 +55,12 @@ class NEG_loss(nn.Module):
 
         input = self.in_embed(input_labes.repeat(1, window_size).contiguous().view(-1))
         output = self.out_embed(out_labels.view(-1))
-
-        noise = Variable(t.Tensor(batch_size * window_size, num_sampled).uniform_(0, self.num_classes - 1).long())
+        
+        noise_sample_count = batch_size * window_size * num_sampled)
+        draw = self.subsample(noise_sample_count)
+        draw.resize((batch_size * window_size, num_sampled))
+        noise = Variable(torch.from_numpy(draw))
+        
         if use_cuda:
             noise = noise.cuda()
         noise = self.out_embed(noise).neg()
