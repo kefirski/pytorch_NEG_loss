e37cf4174e29c7a198580f584dc1ab73e0d34cc8
patrick-dd <patrick.doupe@gmail.com>
2017-05-25 11:47:21 -0400

    removed class variable; added key argument in np.random.choice; added assert for weights; normalised weights

--- NEG_loss/neg.py
+++ NEG_loss/neg.py
@@ -2,19 +2,17 @@ import torch as t
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.nn import Parameter
-
+from numpy.random import choice
 
 class NEG_loss(nn.Module):
-    def __init__(self, num_classes, embed_size, classes = None, weights = None):
+    def __init__(self, num_classes, embed_size, weights = None):
         """
         :param num_classes: An int. The number of possible classes.
         :param embed_size: An int. EmbeddingLockup size
-        :param classes: A list of ints. Class identifiers. None if using
-            uniform sampling
         :param num_sampled: An int. The number of sampled from noise examples
-        :param weights: A list of floats. Class weights. None if using uniform
-            sampling. The weights are calculated prior to estimation and can
-            be of any form, e.g equation (5) in [1]
+        :param weights: A list of non negative floats. Class weights. None if
+            using uniform sampling. The weights are calculated prior to
+            estimation and can be of any form, e.g equation (5) in [1]
         """
 
         super(NEG_loss, self).__init__()
@@ -29,14 +27,18 @@ class NEG_loss(nn.Module):
         self.in_embed.weight = Parameter(t.FloatTensor(self.num_classes, self.embed_size).uniform_(-1, 1))
 
         # FOR SUBSAMPLING
-        self.classes = classes
         self.weights = weights
+        if self.weights is not None:
+            assert min(self.weights) >= 0, "Each weight should be >= 0"
+            # NORMALISING
+            sum_weights = sum(self.weights)
+            self.weights = [ w / sum_weights for w in self.weights ]
 
     def subsample(self, n_samples):
         """
         draws a sample from classes based on weights
         """
-        draw = choice(self.classes, n_samples, self.weights)
+        draw = choice(range(self.num_classes, n_samples, p=self.weights))
         return np.array(draw)
 
     def forward(self, input_labes, out_labels, num_sampled):
@@ -56,7 +58,7 @@ class NEG_loss(nn.Module):
         input = self.in_embed(input_labes.repeat(1, window_size).contiguous().view(-1))
         output = self.out_embed(out_labels.view(-1))
 
-        if self.classes:
+        if self.weights is not None:
             # SUBSAMPLE
             noise_sample_count = batch_size * window_size * num_sampled)
             draw = self.subsample(noise_sample_count)
